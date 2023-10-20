0c42bd15011b61587799eb0dacec5d5b6647c35c
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-20 07:22:41 -0600

    Modify students.js and teachers.js with parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,7 +7,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// Attempt to connect to the database
 pool.connect((err, client, done) => {
   if (err) {
     console.error('Error connecting to the database:', err);
@@ -16,9 +15,7 @@ pool.connect((err, client, done) => {
 
   console.log('Connected to the database');
 
-  // Optionally, you can perform a test query
   client.query('SELECT NOW()', (queryError, result) => {
-    done(); // Release the client back to the pool
 
     if (queryError) {
       console.error('Error running query:', queryError);
@@ -29,15 +26,22 @@ pool.connect((err, client, done) => {
   });
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+const cohortName = process.argv[2];
+const limitValue = process.argv[3] || 5;
+
+pool.query(
+  `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `,
+  [`%${cohortName}%`, limitValue]
+)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
